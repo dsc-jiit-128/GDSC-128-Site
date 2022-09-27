661b37445be713dac39d25e842fd339ee61bc97c
sarthakpant772 <sarthak.pant31@gmail.com>
2022-09-27 23:36:23 +0530

    added .env file

--- server/index.js
+++ server/index.js
@@ -7,10 +7,8 @@ const app = express()
 app.use(express.json({ limit: '64mb' }))
 app.use(cors())
 
-
 const PORT = process.env.PORT || 5000
-const MONGO_URL =
-  'mongodb+srv://gdscj128:mongo2002@cluster0.wxbpuul.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wxbpuul.mongodb.net/?retryWrites=true&w=majority`
 mongoose.set('bufferCommands', false)
 mongoose.connect(MONGO_URL, () => {
   console.log('databse connected')
