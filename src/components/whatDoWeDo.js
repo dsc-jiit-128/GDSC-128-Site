3c4fe3d861c01aec86a4a8f92b3f2b1ab67ac401
TanayK07 <tkedia7@gmail.com>
2022-10-05 12:45:02 +0530

    button alignment fix

--- src/components/whatDoWeDo.js
+++ src/components/whatDoWeDo.js
@@ -6,13 +6,13 @@ export default function WhatDoWeDo() {
     <>
       <Box>
         <Box textAlign={'center'} pt={10}>
-          <Text as={'b'} fontSize="2.5vw" mt={100} color='white'>
+          <Text as={'b'} fontSize="40" mt={100} color='white'>
             What do we do?
           </Text>
         </Box>
         <Grid templateColumns="repeat(2, 1fr)" gap={0}>
           <GridItem w="100%" colSpan={1}>
-            <Box textAlign={'center'} width={'65%'} ml={'25%'}>
+            <Box textAlign={'center'} width={'65%'} ml={'22%'}>
               <Text ml={10} mr={10} mt={10} textAlign="left"   color='white'>
                 With our team's moto being "Giving Direction to Greatness", we
                 help the students to climb mountains on their own, as high as
@@ -20,11 +20,12 @@ export default function WhatDoWeDo() {
                 and ExploreML and various technical workshops, sessions, meetups
                 and fun events as well.
               </Text>
-
-              <Button bgColor={'#3586FF'} size="lg" mt={10}><Text   color='white'>
+</Box>
+              <Box textAlign={'center'} width={'65%'} ml={'14%'}>
+              <Button bgColor={'#3586FF'} size="lg" mt={10}><Text align={'left'}  color='white'>
                 Register as a student</Text>
               </Button>
-            </Box>
+         </Box>
           </GridItem>
         </Grid>
         <Image
