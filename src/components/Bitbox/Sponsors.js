9d9aa6c7415e3272739d67fa2161f8246f6f2a1b
Shivansh-25 <89247605+Shivansh-25@users.noreply.github.com>
2023-01-11 12:26:42 +0530

    added sponsors and teaser part to website

--- src/components/Bitbox/Sponsors.js
+++ src/components/Bitbox/Sponsors.js
@@ -12,23 +12,200 @@ import {
     
   } from '@chakra-ui/react';
 import React from 'react'
+import '../../css/Sponsors.css'
 export default function Sponsors() {
     return (
       <>
-       <Box>
-        <Text     fontSize={{ base: '15px', md: '2.5vw' }}
-                    opacity={[0, 1]}
-                    ml={5}
-                    fontWeight={'semibold'}
-                    color="white"
-                    pl={0}
-                    pt={'2.2vw'}
-                    style={{ textDecoration: 'none' }}
-                    fontFamily={'Gilroy-SemiBold'}
-                    align='center'>
+       <Box margin={'50px'}>
+        <Text fontSize={'64px'} align="center" marginTop={'120px'} textColor='white'>
           Sponsors
         </Text>
-
+        <Grid
+          templateColumns="repeat(4, 0fr)"
+          gap={0}
+          margin='auto'
+          width={'70%'}
+          mt={'150px'}
+        >
+          <GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem>
+          <GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem><GridItem
+            w="275px"
+            h="275px"
+            bg="#161515"
+            border={'1px solid white'}
+            className='sponsor'
+            display={'flex'}
+            justifyContent={'center'}
+            
+          >
+            <Image src= "/logo-google.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+          </GridItem>
+        </Grid>
       </Box>
          
 
--- src/components/Bitbox/Video.js
+++ src/components/Bitbox/Video.js
@@ -11,22 +11,22 @@ import {
     
   } from '@chakra-ui/react';
 import React from 'react'
+import { AspectRatio } from '@chakra-ui/react'
 export default function Video() {
     return (
       <>
-       <Box>
-        <Text     fontSize={{ base: '15px', md: '2.5vw' }}
-                    opacity={[0, 1]}
-                    ml={5}
-                    fontWeight={'semibold'}
-                    color="white"
-                    pl={0}
-                    pt={'2.2vw'}
-                    style={{ textDecoration: 'none' }}
-                    fontFamily={'Gilroy-SemiBold'}
-                    align='center'>
-          Watch the Teaser 
+       <Box margin='auto' marginTop={'100px'}>
+        <Text fontSize={'64px'} textAlign='center' textColor={'white'} >
+          {' '}
+          Watch the teaser{' '}
         </Text>
+        <AspectRatio maxW='1097px' maxH='750px' ratio={1} margin='auto' marginTop={'100px'}>
+          <iframe
+          title='naruto'
+          src='https://www.youtube.com/watch?v=XJ7E5l2YfB0'
+          allowFullScreen
+          />
+        </AspectRatio>
       </Box>
       <Box>
 
