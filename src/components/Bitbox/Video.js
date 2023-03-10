be9102de0eed090e35c75175791987e95a4562c8
Shivansh-25 <89247605+Shivansh-25@users.noreply.github.com>
2023-03-10 18:01:37 +0530

    video added

--- src/components/Bitbox/Video.js
+++ src/components/Bitbox/Video.js
@@ -1,50 +1,49 @@
 import {
-    GridItem,
-    SimpleGrid,
-    Grid,
-    Image,
-    Text,
-    Flex,
-    Link,
-    Box,
-    HStack
-    
-  } from '@chakra-ui/react';
-import React from 'react'
-import { AspectRatio } from '@chakra-ui/react'
+  GridItem,
+  SimpleGrid,
+  Grid,
+  Image,
+  Text,
+  Flex,
+  Link,
+  Box,
+  HStack,
+} from '@chakra-ui/react';
+import React from 'react';
+import { AspectRatio } from '@chakra-ui/react';
 export default function Video() {
-    return (
-      <>
-       <Box margin='auto' marginTop={{base:'10vw',md:'100px'}} mb={{base:'5vw', md:'auto'}}>
-        <Text fontSize={{base:'8vw', md:'6xl'}} textAlign='center' color={'white'} >
+  return (
+    <>
+      <Box
+        margin="auto"
+        marginTop={{ base: '10vw', md: '100px' }}
+        mb={{ base: '5vw', md: 'auto' }}
+      >
+        <Text
+          fontSize={{ base: '8vw', md: '6xl' }}
+          textAlign="center"
+          color={'white'}
+        >
           {' '}
           Watch the teaser{' '}
         </Text>
-        <Box mt='8vw'>
-        <Box className = 'outer2 redd'>
-          <Box className = 'inner2'>
+        <Box mt="8vw">
+          <Box className="outer2 redd">
+            <Box className="inner2"></Box>
+          </Box>
+          <AspectRatio maxW="70%" maxH="50%" margin="auto">
+            <iframe
+              title="naruto"
+              src="https://www.youtube.com/embed/XJ7E5l2YfB0"
+              allowFullScreen
+            />
+          </AspectRatio>
+          <Box className="outer yelloww">
+            <Box className="inner"></Box>
           </Box>
         </Box>
-        <AspectRatio maxW='70%' maxH='50%' margin='auto' >
-          <iframe
-          title='naruto'
-          src='https://www.youtube.com/watch?v=XJ7E5l2YfB0'
-          allowFullScreen
-          />
-        </AspectRatio>
-        <Box className = 'outer yelloww' >
-          <Box className = 'inner' >
-          </Box>
-        </Box>
-        </Box>
       </Box>
-      <Box>
-
-
-
-      </Box>
-
-      </>
-    );
-  }
-  
+      <Box></Box>
+    </>
+  );
+}
