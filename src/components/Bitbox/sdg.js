16c08b0fb3d97074ba1abbc517e6ce16effe8ff8
Shivansh-25 <89247605+Shivansh-25@users.noreply.github.com>
2023-03-21 17:04:05 +0530

    fixed mobile view for themes

--- src/components/Bitbox/sdg.js
+++ src/components/Bitbox/sdg.js
@@ -10,22 +10,23 @@ import '../../css/SDGs.css'
 export default function SDGs() {
     return (
       <>
-             <Box margin={'50px'} mt={{base: '0vw', md:'3vw'}}>
+             <Box margin={'5px'} mt={{base: '0vw', md:'3vw'}}>
         <Text fontSize={{base:'8vw', md:'6xl'}} align="center"  margin = 'auto' marginTop={'120px'} color='white'>
           Project Themes
         </Text>
         <Grid
-          templateColumns="repeat(6, 0fr)"
-          gap={0}
+          templateColumns={{ base: "repeat(3, 0fr)", md: "repeat(6, 0fr)" }}
+          gap={{base:'5px',md:'0px'}}
           alignContent={'center'}
           justifyContent={'center'}
-          ml={{base:'auto',md:'310px'}}
-          mr={{base:'auto',md:'310px'}}
+          ml={{base:'50px',md:'310px'}}
+          mr={{base:'50px',md:'310px'}}
           mt={{base:'3vw',md:'3vw'}}
+          // w={{base:'70%',md:'100%'}}
         >
           <GridItem
-            w=""
-            h=""
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -34,12 +35,12 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal1'>
-            <Image src= "/goal-01_480.png" w={'60%'} h={'60%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-01_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem>
           <GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -48,11 +49,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal2'>
-            <Image src= "/goal-02_480.png" w={'60%'} h={'60%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-02_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -61,11 +62,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal3'>
-            <Image src= "/goal-03_480.png" w={'60%'} h={'60%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-03_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -74,11 +75,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal4'>
-            <Image src= "/goal-04_480.png" w={'60%'} h={'60%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-04_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -87,11 +88,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal5'>
-            <Image src= "/goal-05_480.png" w={'60%'} h={'60%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-05_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -100,11 +101,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal6'>
-            <Image src= "/goal-06_480.png" w={'60%'} h={'60%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-06_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -113,11 +114,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal7'>
-            <Image src= "/goal-07_480.png" w={'60%'} h={'60%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-07_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -126,11 +127,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal8'>
-            <Image src= "/goal-08_480.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-08_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -139,11 +140,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal9'>
-            <Image src= "/goal-09_480.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-09_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -152,11 +153,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal10'>
-            <Image src= "/goal-10_480.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-10_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -165,11 +166,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal11'>
-            <Image src= "/goal-11_480.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-11_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -178,11 +179,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal12'>
-            <Image src= "/goal-12_480.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-12_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -191,11 +192,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal13'>
-            <Image src= "/goal-13_480.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-13_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -204,11 +205,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal14'>
-            <Image src= "/goal-14_480.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-14_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
           </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -217,11 +218,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal15'>
-            <Image src= "/goal-15_480.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-15_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
             </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -230,11 +231,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal16'>
-            <Image src= "/goal-16_480.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-16_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
             </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -243,11 +244,11 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals/goal17'>
-            <Image src= "/goal-17_480.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/goal-17_480.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
             </GridItem><GridItem
-            w="13vw"
-            h="13vw"
+            w={{base:"25vw", md: "13vw"}}
+            h={{base:"25vw", md: "13vw"}}
             bg="#161515"
             border={'1px solid white'}
             className='SDG'
@@ -256,7 +257,7 @@ export default function SDGs() {
             
           >
             <a target="_blank" href='https://sdgs.un.org/goals'>
-            <Image src= "/global-goals.png" w={'50%'} h={'50%'} className='image' margin='auto' position={'relative'}/>
+            <Image src= "/global-goals.png" w={{base:'100%',md:'60%'}} h={{base:'100%',md:'60%'}} className='image' margin='auto' position={'relative'}/>
             </a>
             </GridItem>
         </Grid>
