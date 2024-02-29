53bf0c1969434b4a018ef1135f94ccde4963e7fa
Samarpreet Singh <99521823+Samar-28@users.noreply.github.com>
2024-02-29 20:31:21 +0530

    Update bitbox date

--- src/Pages/Bitbox.js
+++ src/Pages/Bitbox.js
@@ -58,7 +58,7 @@ export default function Bitbox() {
 
   const inputRef = React.useRef(null);
   //Date 20 days 2 hours 5 minutes 30 seconds in future
-  const [date, setDate] = React.useState(new Date("2024-03-16"));
+  const [date, setDate] = React.useState(new Date("2024-03-16T09:00:00"));
   useEffect(() => {
     setTimeout(() => {
       if (inputRef) {
